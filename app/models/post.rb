ac20f15cc249ad8a95caacc8755c5795d0410eb3
Kaia <kbergmanndean@gmail.com>
2021-07-22 11:06:58 -0400

    commit tree 75b64fede842e8654d1d181c5f97c5661c87c863

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence:true
+    validates :content, length: {minimum:250}
+    validates :summary, length: {maximum:250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    validate :must_be_clickbaity
+
+    
+
+    def must_be_clickbaity
+        unless Post.title.include? "Won't Believe"
+            errors.add(:title, "validation error")
+        end
+    end
 end
